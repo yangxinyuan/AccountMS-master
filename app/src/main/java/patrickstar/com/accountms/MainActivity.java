412bf31e395e545f58ec2f28d40e628d7cd120c0
ios18 <2695085493@qq.com>
2017-09-13 16:50:39 +0800

    commit from yangxinyuan

--- app/src/main/java/patrickstar/com/accountms/MainActivity.java
+++ app/src/main/java/patrickstar/com/accountms/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //dxdhuashxsfwsfsvfg
+        //dxdhuashxsfwsfsvfg223
         setContentView(R.layout.addoutaccount);
     }
 }
